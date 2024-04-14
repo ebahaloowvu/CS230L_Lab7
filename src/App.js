bdd5cf0fb17f5b0412dc821f1b9f751733e33889
ebahaloowvu <eb00072@mix.wvu.edu>
2024-04-14 14:27:13 -0400

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>CS 230L</h1>
+    <h2>Section - 01</h2>
+    <p>WVU ID: 800416929</p>
+    <p>Hi I am Ehsan Bahaloo Horeh</p>
+    </div>
+  );
+}
+
+export default App;
